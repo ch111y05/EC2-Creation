279997ba510698ba876b362970b91bae1f5e9d0d
ch111y05 <110796019+ch111y05@users.noreply.github.com>
2023-09-02 11:30:47 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -28,3 +28,8 @@ output "dev_node_instance_id" {
   description = "The ID of the created EC2 instance."
   value       = aws_instance.dev_node.id
 }
+
+output "ami_id" {
+  description = "AMI ID depends on the instance type and region in which you're launching your stack. And IDs can change regularly, such as when an AMI is updated with software updates."
+  value       = aws_ami_from_instance.default.id
+}
